71c790d8b0af24b7757855f0be54898ff591e080
Jairo Narvaez <jaibuu+git@gmail.com>
2016-09-05 18:12:43 -0500

    Made entry store column names

--- app/models/Entry.php
+++ app/models/Entry.php
@@ -2,60 +2,59 @@
 
 namespace Models;
 
-class Entry extends \Phalcon\Mvc\Model {
-	public $encrypted_data;
+class Entry extends \Phalcon\Mvc\Model {    
+    public $encrypted_data;
+    
+    public $date;
+    
+    private $form_data;
+    
+    private function getKey(){  
+        $app = new \Application\Micro(); 
+        $clientId = $app->request->getHeader('API_ID');
+    
+        return $app->config->data->clients[$clientId]->crypt_key;
+    }
+    
+    private function decrypt(){
+        $decoded = base64_decode($this->encrypted_data);
 
-	public $date;
+        $iv = mb_substr($decoded, 0, 16, '8bit');   
 
-	private $name;
+        $ciphered = mb_substr($decoded, 16, null, '8bit'); 
 
-	private function getKey(){
-		$app = new \Application\Micro();
-		$clientId = $app->request->getHeader('API_ID');
+        $decrypted = rtrim(
+            mcrypt_decrypt(
+                MCRYPT_RIJNDAEL_128,
+                $this->getKey(),
+                $ciphered,
+                'ctr',
+                $iv
+            ),
+            "\0"
+        );        
 
-		return $app->config->data->clients[$clientId]->crypt_key;
+        $data = json_decode($decrypted, true);
+    
+        $this->form_data = $data;  
+    }
+    
+    public function save($data = null, $whiteList = null) { 
+        $this->decrypt();
 
-	}
+        $file_name = "entries-" . date('Y-m-d') . ".csv";
+        $this->form_data['ServerDate'] = $this->date;
 
-	private function decrypt(){
+        $file_exists = file_exists($file_name);
 
-		$decoded = base64_decode($this->encrypted_data);
+        $fp = fopen($file_name, "a");
 
+        if(!$file_exists){
+            fputcsv($fp,array_keys($this->form_data));
+        }
 
-	    $iv = mb_substr($decoded, 0, 16, '8bit');
-	    $ciphered = mb_substr($decoded, 16, null, '8bit');
-	      
-	    $decrypted = rtrim(
-	        mcrypt_decrypt(
-	            MCRYPT_RIJNDAEL_128,
-	            $this->getKey(),
-	            $ciphered,
-	            'ctr',
-	            $iv
-	        ),
-	        "\0"
-	    );        
+        fputcsv($fp,$this->form_data);
 
-		
-		$data = json_decode($decrypted, true);
-
-		//assigning decrypted values as properties of Entry
-		foreach ($data as $key => $value) {
-		 $this->{$key} = $value;
-		}
-
-	}
-
-	public function save($data = null, $whiteList = null) {
-		$this->decrypt();
-
-        $fp = fopen("entries-" . date('Y-m-d') . ".csv", "a");
-
-        fputcsv($fp,array(
-        	$this->name,
-        	$this->date
-        ));
-
-        fclose($fp);
-	}
+        fclose($fp);    
+    }
 }
