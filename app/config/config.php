a109ce51370928a20155acbb237a4514b13383d8
Jairo Narvaez <jaibuu+git@gmail.com>
2016-09-07 18:12:19 -0500

    file structure and comments change

--- app/config/config.php
+++ app/config/config.php
@@ -15,9 +15,9 @@ $settings = array(
 	),
 	'data' => array(
 		'clients' => array(
-			'1' => array (
-				'private_key' => '8c484c8c89547cncy4mc8nzxbcvi4eba66e10fba74dbf9e99c22f',
-				'crypt_key' => 'wbHPNKi@t-4t@k(!b9xh^gVJ&ywi0z$5',
+			'1' => array ( //sample client id
+				'private_key' => '8c484c8c89547cncy4mc8nzxbcvi4eba66e10fba74dbf9e99c22f', //sample key
+				'crypt_key' => 'wbHPNKi@t-4t@k(!b9xh^gVJ&ywi0z$5', //sample key
 			)
 
 		)
